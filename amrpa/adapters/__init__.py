6b087d2a777c4e024b518caf890d0208cdabbdaf
Yasar Mulani <mulaniyhofficial@gmail.com>
2026-04-13 18:37:47 +0530

    Update __init__.py

--- amrpa/adapters/__init__.py
+++ amrpa/adapters/__init__.py
@@ -1,7 +1,18 @@
 from .encoder import apply_amrpa_to_encoder, reset_encoder, EncoderAMRPAState
 from .decoder import apply_amrpa_to_decoder, reset_decoder, DecoderAMRPAState
+from .universal import apply_amrpa_universal, UniversalAMRPAState
+from .registry import MODEL_REGISTRY, get_model_info, is_supported, get_arch
 
 __all__ = [
+    # Universal (recommended)
+    'apply_amrpa_universal',
+    'UniversalAMRPAState',
+    # Registry
+    'MODEL_REGISTRY',
+    'get_model_info',
+    'is_supported',
+    'get_arch',
+    # Legacy specific adapters
     'apply_amrpa_to_encoder',
     'reset_encoder',
     'EncoderAMRPAState',
